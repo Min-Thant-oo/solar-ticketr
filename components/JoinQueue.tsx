53ec9bee65fdf804258df244c69f27ebe85a459f
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-12-05 20:12:15 -0600

    created Join Queue functionality

--- components/JoinQueue.tsx
+++ components/JoinQueue.tsx
@@ -2,23 +2,109 @@ import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel'
 import { useToast } from '@/hooks/use-toast';
 import { useMutation, useQuery } from 'convex/react';
+import { ConvexError } from 'convex/values';
 import React from 'react'
+import Spinner from './Spinner';
+import { WAITING_LIST_STATUS } from '@/convex/constant';
+import { Clock, OctagonXIcon } from 'lucide-react';
 
 const JoinQueue = ({ 
     eventId, 
     userId 
 }: {
     eventId: Id<'events'>;
-    userId: Id<'users'>;
+    userId: string;
 }) => {
 
     const { toast } = useToast();
     const joinWaitingList = useMutation(api.events.joinWaitingList);
     const queuePosition = useQuery(api.waitingList.getQueuePosition, { eventId, userId });
     const userTicket = useQuery(api.tickets.getUserTicketForEvent, { eventId, userId });
+    const availablity = useQuery(api.events.getEventAvailability, { eventId });
+    const event = useQuery(api.events.getById, { eventId });
+    const isEventOwner = userId === event?.userId;
+
+    const handleJoinQueue = async () => {
+        try {
+            const result = await joinWaitingList({ eventId, userId });
+            if(result.success) {
+                console.log('Successfully joined waiting list');
+                toast({
+                    title: result.message,
+                    duration: 5000,
+                });
+            }
+        } catch (error) {
+            if(
+                error instanceof ConvexError &&
+                error.message.includes('joined the waiting list too many times')
+            ) {
+                toast({
+                    variant: 'destructive',
+                    title: 'Slow down there!',
+                    description: error.data,
+                    duration: 5000,
+                });
+            } else {
+                console.error('Error joing waiting list:', error);
+                toast({
+                    variant: 'destructive',
+                    title: 'Uh oh! Something went wrong.',
+                    description: 'Failed to join queue. Please try again later.',
+                });
+            }
+        }
+    }
+
+    if(queuePosition === undefined || availablity === undefined || !event) {
+        return <Spinner />
+    }
+
+    if(userTicket) {
+        return null;
+    }
+
+    const isPastEvent = event.eventDate < Date.now();
 
     return (
-        <div>JoinQueue</div>
+        <div>
+            {(!queuePosition || 
+                queuePosition.status === WAITING_LIST_STATUS.EXPIRED ||
+                (queuePosition.status === WAITING_LIST_STATUS.OFFERED &&
+                    queuePosition.offerExpiresAt &&
+                    queuePosition.offerExpiresAt <= Date.now()
+                )
+            ) && (
+                <>
+                    {isEventOwner ? (
+                        <div className="flex items-center justify-center gap-2 w-full py-3 px-4 bg-gray-100 text-gray-700 rounded-lg">
+                            <OctagonXIcon className='w-5 h-5' />
+                            <span>You cannot buy a ticet for your own event</span>
+                        </div>
+                    ) : (
+                        isPastEvent ? (
+                            <div className="flex items-center justify-center gap-2 w-full py-3 px-4 bg-gray-100 text-gray-700 rounded-lg">
+                                <Clock className='w-5 h-5' />
+                                <span>Event has ended.</span>
+                            </div>
+                        ) : availablity.purchasedCount >= availablity?.totalTickets ? (
+                            <div className="text-center p-4">
+                                <p className="text-lg font-sembold text-red-600">
+                                    Sorry, this event is sold out.
+                                </p>
+                            </div>
+                        ) : (
+                            <button
+                                className='w-full bg-blue-600 text-white px-6 py-3 rounded-lg font-medium hover:bg-blue-700 transition-colors duration-200 shadow-md flex items-center justify-center disabled:bg-gray-400 disabled:cursor-not-allowed'
+                                onClick={handleJoinQueue}
+                            >
+                                Buy ticket
+                            </button>
+                        )
+                    )}
+                </>
+            )}
+        </div>
     )
 }
 
