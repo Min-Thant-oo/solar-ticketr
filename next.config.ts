97e84def0295be2a339333fe382fb9d39f5b9ba3
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-12-09 03:16:57 -0600

    event creation form done

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: "valiant-loris-158.convex.cloud", protocol: "https" },
+    ],
+  },
 };
 
 export default nextConfig;
