e7b487dc90d06bf9c1c242b3904b68a016769a7f
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-12-09 15:31:28 -0600

    buy-ticket functionality done

--- components/PurchaseTicket.tsx
+++ components/PurchaseTicket.tsx
@@ -8,6 +8,7 @@ import { Ticket } from 'lucide-react';
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 import ReleaseTicket from './ReleaseTicket';
+import { createStripeCheckoutSession } from '@/app/actions/createStripeCheckoutSession';
 
 const PurchaseTicket = ({ eventId }: { eventId: Id<'events'> }) => {
     const router = useRouter();
@@ -50,9 +51,24 @@ const PurchaseTicket = ({ eventId }: { eventId: Id<'events'> }) => {
 
     // create stripe checkout
     const handlePurchase = async () => {
-        if(!user || !queuePosition || queuePosition.status !== 'offered') {
-            return null;
+        if (!user) return;
+    
+        try {
+            setIsLoading(true);
+            const { sessionUrl } = await createStripeCheckoutSession({eventId});
+
+            if (sessionUrl) {
+                router.push(sessionUrl);
+            }
+        } catch (error) {
+            console.error("Error creating checkout session:", error);
+        } finally {
+            setIsLoading(false);
         }
+    };
+    
+    if (!user || !queuePosition || queuePosition.status !== "offered") {
+        return null;
     }
     
     return (
